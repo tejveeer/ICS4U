3e504c22ca2863aa87aa0982e19f41811501b213
Tejveer <tejveerrehsi@gmail.com>
2023-02-03 23:02:53 -0500

    setup, alongside first exercise

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ICS4U
+This repository will be used to store all the content that I'll be going through within this course. The purpose of this is just to organize all the things neatly.
+
+## Current Plan
+1. Work on learning web dev (html, css/scss, javascript/typescript, bootstrap, react, nodejs) for the purpose of volunteer work and personal blog. (Deadline: April)
+2. Learn rust and how to make a parser/lexer for the purpose of creating my own database.
+   1. Perhaps research Ethereum smart contracts and how to work with them with rust.
+3. Continue my study of functional programming languages and category theory
+
+This indeed sounds quite a lot of work, and possibly out of scope for the current time, but I certainly can give it a try. The main goal, however, will be #1, unless plans change. The course summative should be trivial enough to complete during class time so it shouldn't be a major problem.
---
+++ python_exercises/assignment_0.py
@@ -0,0 +1,66 @@
+
+# question 1
+def shipping_cost(weight: int) -> float:
+    extra = 0
+
+    if 25 <= weight < 50:
+        extra = 10
+    elif 50 <= weight < 100:
+        extra = 15
+    elif 100 <= weight:
+        extra = 30
+    
+    return extra + (0.1 * weight)
+
+# question 2
+def even_odd(array: list[int]) -> tuple[int, int]:
+    return (
+        sum(i for i in array if i % 2 == 0),
+        sum(i for i in array if i % 2 == 1)
+    )
+
+# question 3:
+class Item:
+    def __init__(self, name: str, section: str, price: float) -> None:
+        # private attributes (can use double underscore for more safety, but unnecessary)
+        self._name = name
+        self._section = section
+        self._price = price
+    
+    def __imul__(self, other: float):
+        self._price *= other
+        return self
+    
+    def __iadd__(self, other: float):
+        self._price += other
+        return self
+    
+    def __str__(self) -> str:
+        capitalized_name = self._name.capitalize()
+        return f"{capitalized_name} costs {self._price} and can be found in {self._section}."
+
+# test cases
+if __name__ == '__main__':
+    # question 1
+    print(*(
+        f"{amount} pound -> ${shipping_cost(amount)}"
+        for amount in (30, 56, 110)
+    ), sep='\n')
+
+    # question 2
+    print('\n', *(
+        f"{ls} -> {even_odd(ls)}"
+        for ls in (
+            [1, 4, 3, 6, 2],
+            [2, 6, 1, 3, 5]
+        )
+    ), sep='\n')
+
+    # question 3
+    percent = lambda n: n / 100
+    milk = Item('milk', 'groceries', 4.99)
+    
+    milk += 10 # adds 10 dollars
+    milk *= percent(120) # increases by 120 percent
+
+    print(milk)
