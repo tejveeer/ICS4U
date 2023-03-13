2e2e17f451b5e9f2f89880046072db9128627a22
Tejveer Rehsi <tejveerrehsi@gmail.com>
2023-03-13 11:59:23 -0400

    Github seems to have messed up my indentation?

--- webdev_exercises/p1/script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("click", (e) => {
-    const [width, height] = [e.clientX, e.clientY];
-    const circle = document.getElementById("circle");
-
-    circle.style.transform = `translate(${width - circle.offsetLeft - 25}px, ${height - circle.offsetTop - 25}px)`;
-});
-
